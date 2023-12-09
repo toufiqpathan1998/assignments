e558cb9f5211c13d4a566c04dcc312f8bc1ab2da
toufiqpathan1998 <toufiqpathan1998@gmail.com>
2023-12-09 11:30:43 +0530

    find largest num

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,13 @@
 */
 
 function findLargestElement(numbers) {
+    let biggestNumber = numbers[0]
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > biggestNumber) {
+            biggestNumber = numbers[i]
+        }
+    }
+    return biggestNumber
     
 }
 
